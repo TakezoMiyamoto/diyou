f1fd14ffff4884de8545fc074261aaab66664ea9
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-06-30 16:34:34 +0900

    #2 Login with facebook account

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -242,6 +242,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, '1701157326813407', 'dc668b6f8ef3ae38f0f6cfe305ac8733', :scope => 'email', :display => 'popup'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
