61a2fe7153e5e229337af0669ac2331c15e85ad3
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-07-06 00:24:51 +0900

    #7 add nested in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,7 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many :ingredients
   has_many :steps
+
+  accepts_nested_attributes_for :ingredients, allow_destroy: true # この行を追記
+  accepts_nested_attributes_for :steps, allow_destroy: true # この行を追記
 end
