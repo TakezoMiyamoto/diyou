ca789effa2037ccc1305c3e21ff656e553c01190
Takezo Miyamoto <takezo.m.1987@gmail.com>
2016-10-09 23:24:17 +0900

    #41 changed column data type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919022736) do
+ActiveRecord::Schema.define(version: 20161009142058) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -71,8 +71,8 @@ ActiveRecord::Schema.define(version: 20160919022736) do
     t.string   "category",    limit: 255
     t.string   "main_image",  limit: 255
     t.string   "level",       limit: 255
-    t.integer  "time",        limit: 4
-    t.integer  "cost",        limit: 4
+    t.string   "time",        limit: 255
+    t.string   "cost",        limit: 255
     t.string   "youtube_url", limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
